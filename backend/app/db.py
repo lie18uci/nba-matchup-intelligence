19d893632c016af075df746e3f855260f8f44be6
Eric Li <lie18@uci.edu>
2026-04-27 11:56:57 -0700

    Connect FastAPI backend to Postgres

--- backend/app/db.py
+++ backend/app/db.py
@@ -5,7 +5,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-load_dotenv()
+load_dotenv(override=True)
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
